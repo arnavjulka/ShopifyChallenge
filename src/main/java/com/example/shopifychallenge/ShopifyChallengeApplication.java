834ea71d4eb286565fb36418ad0738c7b49454ab
Arnav Julka <ajulka@ncsu.edu>
2022-01-19 01:09:14 -0500

    basic stuff done

---
+++ src/main/java/com/example/shopifychallenge/ShopifyChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopifychallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopifyChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopifyChallengeApplication.class, args);
+    }
+
+}
